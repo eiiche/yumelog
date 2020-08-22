a45d4d1e1f3394e1a1c409c7afdbccc733174709
eache <ekugio.0809@gmail.com>
2020-08-22 17:28:25 +0900

    update

--- app/Http/Controllers/Admin/ManageDiariesPageController.php
+++ app/Http/Controllers/Admin/ManageDiariesPageController.php
@@ -9,28 +9,31 @@ use Illuminate\Http\Request;
 
 class ManageDiariesPageController extends Controller
 {
+    //管理画面 Diaryテーブル一覧表示
     public function index(){
         $diaries = Diary::latest()->get();
         return view("admin.manage_diaries",["diaries"=>$diaries]);
     }
 
+    //管理画面　Diaryテーブル検索処理
     public function search(DiarySearchRequest $request){
+        //検索値を用意
         $search_text = $request->search_text;
         $search_id = $request->id;
         $since_date = $request->since_date;
         $until_date = $request->until_date;
+        //検索
         $diaries =
             Diary::
-//                //日記ID・投稿文検索
-//            where("id","like","%".$search_text."%")
-//            ->orWhere("text","like","%".$search_text."%")
-//                //投稿者ID検索
-//            ->where("author_id","=",$search_id)
-//                //登校日検索
-//            ->whereBetween("created_at",[$since_date,$until_date])->get();
-            whereBetween("created_at",[$since_date,$until_date])->get();
+                //日記ID・投稿文検索
+            where("id","like","%".$search_text."%")
+            ->orWhere("text","like","%".$search_text."%")
+                //投稿者ID検索
+            ->where("author_id","=",$search_id)
+                //登校日検索
+            ->whereBetween("created_at",[$since_date,$until_date])->get();
 
-
+        //ビューに渡す
         return view("admin.manage_diaries",["diaries"=>$diaries]);
     }
 }
